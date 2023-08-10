343d91b052174cabaf2c81cf8f0d32d8b82aa182
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-08-10 12:21:37 +0200

    signup

---
+++ @types/index.d.ts
@@ -0,0 +1,11 @@
+declare module '@types/bcrypt';
+declare module '@types/expres';
+declare module '@types/cors';
+declare module '@types/uuid';
+declare module '@types/jsonwebtoken'
+declare module 'express';
+declare module 'http-errors';
+declare interface IExampleData {
+    name: string;
+    id: string;
+  }
