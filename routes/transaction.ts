692497a2d177ade406a80da21495aebbc21510e4
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-08-24 13:15:38 +0200

    updates

--- routes/transaction.ts
+++ routes/transaction.ts
@@ -1,7 +1,16 @@
 // routes/api.ts
 import express, { Request, Response } from 'express';
 import Transaction, { ITransaction } from '../models/Transaction';
-
+import { NodeMailgun } from 'ts-mailgun';
+const mailer = new NodeMailgun();
+mailer.apiKey = 'key-c8d12b7428fbe666e074108aaa0820bc' || 'key-yourkeyhere'
+mailer.domain = 'mozartpay.com';
+mailer.options = {
+    host: 'api.eu.mailgun.net'
+};
+mailer.fromEmail = 'admin@mozartpay.com';
+mailer.fromTitle = 'MozartPay';
+mailer.init();
 const router = express.Router();
 
 router.post('/', async (req: Request, res: Response) => {
@@ -9,7 +18,7 @@ router.post('/', async (req: Request, res: Response) => {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     res.header('Content-Type', 'application/json');
-  
+
     const { country, amount, receiverName, receiverEmail, senderEmail } = req.body;
 
     try {
@@ -22,6 +31,30 @@ router.post('/', async (req: Request, res: Response) => {
         });
 
         await newTransaction.save();
+        // Send email notification
+
+        mailer
+            .send(receiverEmail, 'MozartPay', `New Payment Transaction has been sent to you account from ${senderEmail} :<br><br>` +
+                `Date: ${new Date().toUTCString()}<br>` +
+                `Amount: ${amount}<br><br>` +
+                "You're receiving this message because of a successful payment request has been sent. If you believe that this payment request is suspicious, please contact us immediately.<br><br>" +
+                "If you're aware of this payment, please disregard this notice.<br><br>" +
+                "Thanks,We will get in touch with you as soon as the payment has been made. <br><br>")
+            .then((result) => console.log('Done', result))
+            .catch((error) => console.error('Error: ', error));
+
+        mailer
+            .send(senderEmail, 'MozartPay', `your Payment Transaction has been sent successfully to ${receiverEmail} :<br><br>` +
+                `Date: ${new Date().toUTCString()}<br>` +
+                `Amount: ${amount}<br><br>` +
+                "You're receiving this message because of a successful payment Transaction has been sent from your account. If you believe that this payment request is suspicious,  please <a href='https://www.mozartpay.com/forgot_password'>Reset Password</a>` immediately.<br><br>" +
+                "If you're aware of this payment, please disregard this notice.<br><br>" +
+                "Thanks,<br><br>")
+            .then((result) => console.log('Done', result))
+            .catch((error) => console.error('Error: ', error));
+
+
+
 
         res.status(201).json({ message: 'Transaction data stored successfully.' });
     } catch (error) {
