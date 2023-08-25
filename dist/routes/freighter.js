afe3e10c311f65ef398e77a810fd564eda59f4f3
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-08-25 16:35:21 +0200

    updates

--- dist/routes/freighter.js
+++ dist/routes/freighter.js
@@ -27,54 +27,51 @@ router.post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         if (!email || !amount) {
             return res.status(400).json({ error: "Email and amount are required." });
         }
-        // Set up Stellar SDK
-        const server = new stellar_sdk_1.default.Server("https://horizon-testnet.stellar.org");
-        const sourceKeys = stellar_sdk_1.default.Keypair.fromSecret("SCZANGBA5YHTNYVVV4C3U252E2B6P6F5T3U6MM63WBSBZATAQI3EBTQ4");
-        const destinationId = "GA2C5RFPE6GCKMY3US5PAB6UZLKIGSPIUKSLRB6Q723BM2OARMDUYEJ5";
-        // Build the transaction
-        let transaction;
         try {
-            const destinationAccount = yield server.loadAccount(destinationId);
-            const sourceAccount = yield server.loadAccount(sourceKeys.publicKey());
-            transaction = new stellar_sdk_1.default.TransactionBuilder(sourceAccount, {
-                fee: stellar_sdk_1.default.BASE_FEE,
-                networkPassphrase: stellar_sdk_1.default.Networks.TESTNET,
-            })
-                .addOperation(stellar_sdk_1.default.Operation.payment({
-                destination: destinationId,
-                asset: stellar_sdk_1.default.Asset.native(),
-                amount: amount, // Use the amount from the frontend
-            }))
-                .addMemo(stellar_sdk_1.default.Memo.text("Test Transaction"))
-                .setTimeout(180)
-                .build();
+            if (yield (0, freighter_api_1.isConnected)()) {
+                // Handle your app authorization logic
+                if (!(yield (0, freighter_api_1.isAllowed)())) {
+                    yield (0, freighter_api_1.setAllowed)();
+                }
+                // Get user information (public key)
+                const userInfo = yield (0, freighter_api_1.getUserInfo)();
+                const { publicKey } = userInfo;
+                // Get network information
+                const network = yield (0, freighter_api_1.getNetwork)();
+                // Set up Stellar SDK
+                const server = new stellar_sdk_1.default.Server("https://horizon-testnet.stellar.org");
+                const sourceKeys = stellar_sdk_1.default.Keypair.fromSecret("SCZANGBA5YHTNYVVV4C3U252E2B6P6F5T3U6MM63WBSBZATAQI3EBTQ4");
+                const destinationId = "GA2C5RFPE6GCKMY3US5PAB6UZLKIGSPIUKSLRB6Q723BM2OARMDUYEJ5";
+                // Build the transaction
+                let transaction;
+                const destinationAccount = yield server.loadAccount(destinationId);
+                const sourceAccount = yield server.loadAccount(sourceKeys.publicKey());
+                transaction = new stellar_sdk_1.default.TransactionBuilder(sourceAccount, {
+                    fee: stellar_sdk_1.default.BASE_FEE,
+                    networkPassphrase: stellar_sdk_1.default.Networks.TESTNET,
+                })
+                    .addOperation(stellar_sdk_1.default.Operation.payment({
+                    destination: destinationId,
+                    asset: stellar_sdk_1.default.Asset.native(),
+                    amount: amount, // Use the amount from the frontend
+                }))
+                    .addMemo(stellar_sdk_1.default.Memo.text("Test Transaction"))
+                    .setTimeout(180)
+                    .build();
+                // Convert transaction to XDR string
+                const transactionXDR = transaction.toXDR("base64");
+                // Use the transactionXDR with the Freighter API
+                const signedTransaction = yield (0, freighter_api_1.signTransaction)(transactionXDR, {
+                    network,
+                    accountToSign: publicKey,
+                });
+                // Respond with the signed transaction or other data
+                res.status(200).json({ signedTransaction });
+            }
         }
         catch (error) {
             console.error("Error building transaction:", error);
             return res.status(500).json({ error: "An error occurred while building the transaction." });
-        }
-        if (yield (0, freighter_api_1.isConnected)()) {
-            // Handle your app authorization logic
-            if (!(yield (0, freighter_api_1.isAllowed)())) {
-                yield (0, freighter_api_1.setAllowed)();
-            }
-            // Get user information (public key)
-            const userInfo = yield (0, freighter_api_1.getUserInfo)();
-            const { publicKey } = userInfo;
-            // Get network information
-            const network = yield (0, freighter_api_1.getNetwork)();
-            // Convert transaction to XDR string
-            const transactionXDR = transaction.toXDR("base64");
-            // Use the transactionXDR with the Freighter API
-            const signedTransaction = yield (0, freighter_api_1.signTransaction)(transactionXDR, {
-                network,
-                accountToSign: publicKey,
-            });
-            // Respond with the signed transaction or other data
-            res.status(200).json({ signedTransaction });
-        }
-        else {
-            res.status(400).json({ error: "Freighter is not installed." });
         }
     }
     catch (error) {
--- routes/freighter.ts
+++ routes/freighter.ts
@@ -1,91 +1,97 @@
 import { Router, Request, Response } from "express";
 import {
-  isConnected,
-  isAllowed,
-  setAllowed,
-  getUserInfo,
-  signTransaction,
-  getNetwork,
+    isConnected,
+    isAllowed,
+    setAllowed,
+    getUserInfo,
+    signTransaction,
+    getNetwork,
 } from "@stellar/freighter-api";
-import StellarSdk from "stellar-sdk"; 
+import StellarSdk from "stellar-sdk";
 
 const router = Router();
 
 router.post('/', async (req: Request, res: Response) => {
-  try {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    res.header('Content-Type', 'application/json');
-  
-    // Get email and amount from the frontend request body
-    const { email, amount } = req.body;
-
-    if (!email || !amount) {
-      return res.status(400).json({ error: "Email and amount are required." });
-    }
-
-    // Set up Stellar SDK
-    const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
-    const sourceKeys = StellarSdk.Keypair.fromSecret("SCZANGBA5YHTNYVVV4C3U252E2B6P6F5T3U6MM63WBSBZATAQI3EBTQ4");
-    const destinationId = "GA2C5RFPE6GCKMY3US5PAB6UZLKIGSPIUKSLRB6Q723BM2OARMDUYEJ5";
-
-    // Build the transaction
-    let transaction;
     try {
-      const destinationAccount = await server.loadAccount(destinationId);
-      const sourceAccount = await server.loadAccount(sourceKeys.publicKey());
+        res.header("Access-Control-Allow-Origin", '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+        res.header('Content-Type', 'application/json');
 
-      transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
-        fee: StellarSdk.BASE_FEE,
-        networkPassphrase: StellarSdk.Networks.TESTNET,
-      })
-        .addOperation(
-          StellarSdk.Operation.payment({
-            destination: destinationId,
-            asset: StellarSdk.Asset.native(),
-            amount: amount, // Use the amount from the frontend
-          }),
-        )
-        .addMemo(StellarSdk.Memo.text("Test Transaction"))
-        .setTimeout(180)
-        .build();
+        // Get email and amount from the frontend request body
+        const { email, amount } = req.body;
+
+        if (!email || !amount) {
+            return res.status(400).json({ error: "Email and amount are required." });
+        }
+        try {
+
+            if (await isConnected()) {
+                // Handle your app authorization logic
+                if (!(await isAllowed())) {
+                    await setAllowed();
+                }
+
+                // Get user information (public key)
+                const userInfo = await getUserInfo();
+                const { publicKey } = userInfo;
+
+                // Get network information
+                const network = await getNetwork();
+
+
+                // Set up Stellar SDK
+                const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
+                const sourceKeys = StellarSdk.Keypair.fromSecret("SCZANGBA5YHTNYVVV4C3U252E2B6P6F5T3U6MM63WBSBZATAQI3EBTQ4");
+                const destinationId = "GA2C5RFPE6GCKMY3US5PAB6UZLKIGSPIUKSLRB6Q723BM2OARMDUYEJ5";
+
+                // Build the transaction
+                let transaction;
+
+                const destinationAccount = await server.loadAccount(destinationId);
+                const sourceAccount = await server.loadAccount(sourceKeys.publicKey());
+
+                transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
+                    fee: StellarSdk.BASE_FEE,
+                    networkPassphrase: StellarSdk.Networks.TESTNET,
+                })
+                    .addOperation(
+                        StellarSdk.Operation.payment({
+                            destination: destinationId,
+                            asset: StellarSdk.Asset.native(),
+                            amount: amount, // Use the amount from the frontend
+                        }),
+                    )
+                    .addMemo(StellarSdk.Memo.text("Test Transaction"))
+                    .setTimeout(180)
+                    .build();
+
+                // Convert transaction to XDR string
+                const transactionXDR = transaction.toXDR("base64");
+
+                // Use the transactionXDR with the Freighter API
+                const signedTransaction = await signTransaction(transactionXDR, {
+                    network,
+                    accountToSign: publicKey,
+                });
+
+                // Respond with the signed transaction or other data
+                res.status(200).json({ signedTransaction });
+
+            }
+        } catch (error) {
+            console.error("Error building transaction:", error);
+            return res.status(500).json({ error: "An error occurred while building the transaction." });
+        }
+
+
+
+
+
     } catch (error) {
-      console.error("Error building transaction:", error);
-      return res.status(500).json({ error: "An error occurred while building the transaction." });
+        console.error("Error handling payment:", error);
+        res.status(500).json({ error: "An error occurred." });
     }
-
-    if (await isConnected()) {
-      // Handle your app authorization logic
-      if (!(await isAllowed())) {
-        await setAllowed();
-      }
-
-      // Get user information (public key)
-      const userInfo = await getUserInfo();
-      const { publicKey } = userInfo;
-
-      // Get network information
-      const network = await getNetwork();
-
-      // Convert transaction to XDR string
-      const transactionXDR = transaction.toXDR("base64");
-
-      // Use the transactionXDR with the Freighter API
-      const signedTransaction = await signTransaction(transactionXDR, {
-        network,
-        accountToSign: publicKey,
-      });
-
-      // Respond with the signed transaction or other data
-      res.status(200).json({ signedTransaction });
-    } else {
-      res.status(400).json({ error: "Freighter is not installed." });
-    }
-  } catch (error) {
-    console.error("Error handling payment:", error);
-    res.status(500).json({ error: "An error occurred." });
-  }
 });
 
 export default router;
