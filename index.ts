48ed5fd0fe2483f593937a90f80f79c370b21db8
olvisgil <olvisgil@gmail.com>
2024-09-20 23:36:55 +0200

    SEP2

--- index.ts
+++ index.ts
@@ -58,6 +58,48 @@ connectToDB();
 // Serve the stellar.toml file
 app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
 
+interface User {
+  stellar_address: string;
+  account_id: string;
+  memo_type?: string;
+  memo?: string;
+}
+
+const users: { [key: string]: User } = {
+  'bob': {
+    stellar_address: 'bob*mozartpay.com',
+    account_id: 'Gxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    memo_type: 'id',
+    memo: '123456789',
+  },
+  'alice': {
+    stellar_address: 'alice*mozartpay.com',
+    account_id: 'Gyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',
+  }
+};
+
+app.get('/federation', (req, res) => {
+  const { q, type } = req.query;
+
+  if (!q || !type) {
+    return res.status(400).json({ error: 'Missing query parameters' });
+  }
+
+  const qValue = Array.isArray(q) ? q[0] : q;
+  if (typeof qValue === 'string') {
+    const [username, domain] = qValue.split('*');
+    
+    if (domain !== 'mozartpay.com' || !users[username]) {
+      return res.status(404).json({ error: 'Stellar address not found' });
+    }
+
+    return res.status(200).json(users[username]);
+  } else {
+    return res.status(400).json({ error: 'Invalid query parameter' });
+  }
+});
+
+
 // Default Route
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, Mozart Typescript Node.js server!");
